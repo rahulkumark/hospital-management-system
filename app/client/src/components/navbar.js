47a080fe40d910894594b51123c2646e135e456c
mohd rafi uddin <mohdrafi188@gmail.com>
2018-06-25 14:20:08 +0530

    created navbar component that contain a login / signup button.

---
+++ app/client/src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+  render(){
+    const navbarStyle = {
+      textDecoration:'none',
+      border: '1px solid blue',
+      borderRadius:'5px',
+      padding:'10px',
+      marginRight:'40px',
+      float:'right'
+    }
+    return(
+      <div>
+       <a href='#' style={navbarStyle}> Login / Signup </a>
+      </div>
+    );
+  }
+}
+export default Navbar;
--- app/client/src/index.js
+++ app/client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Title from './components/title.js';
+import Navbar from './components/navbar.js';
 
 class App extends React.Component{
   render(){
     return (
-      <Title />
+      <div>
+        <Title />
+        <Navbar />
+      </div>
     );
   }
 }
